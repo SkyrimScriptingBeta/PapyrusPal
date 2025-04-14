ea30128c48b5247300c644eb8c2926bdf1da0e6e
Mrowr Purr <mrowr.purr@gmail.com>
2025-04-13 19:27:21 -0700

    Fantastic!

--- src/qt_helpers/fonts.py
+++ src/qt_helpers/fonts.py
@@ -3,9 +3,7 @@ from PySide6.QtGui import QFontDatabase
 
 def load_fonts():
     # Generated using BASH script (found in [root]/resources/qt/font-names.sh)
-    custom_font_names = [
-        "FuckinGwenhwyfar",
-    ]
+    custom_font_names = ["FiraCode", "FiraCodeiScript"]
 
     for font_name in custom_font_names:
         print(f"Loading {font_name}")
